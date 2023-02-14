8161ce5d1cae98802115abfc3885c841a9777793
fpaiva <felipe.paiva@nttdata.com>
2023-02-14 14:25:12 -0300

    Destination

--- app-countries/src/feature/countries/services/countries.service.ts
+++ app-countries/src/feature/countries/services/countries.service.ts
@@ -4,39 +4,39 @@ import { destination } from '../providers/destination.provider';
 
 @Injectable()
 export class CountriesService {
-  async getCountries(lang: string): Promise<any> {
-    try {
-      const response = await axios.get(
-        `https://restcountries.com/v2/lang/${lang}`,
-      );
-      return response;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  // async getCountries(lang: string) {
-  //   return new Promise((resolve, reject) => {
-  //     destination
-  //       .then((destinationObject) => {
-  //         const url = destinationObject.url + lang;
-
-  //         this.httpService(url)
-  //           .then((data) => {
-  //             console.log('0');
-  //             resolve(data);
-  //           })
-  //           .catch((error) => {
-  //             console.log('1');
-  //             reject(error.response.data);
-  //           });
-  //       })
-  //       .catch((err) => {
-  //         console.log('2');
-  //         reject(err);
-  //       });
-  //   });
+  // async getCountries(lang: string): Promise<any> {
+  //   try {
+  //     const response = await axios.get(
+  //       `https://restcountries.com/v2/lang/${lang}`,
+  //     );
+  //     return response;
+  //   } catch (error) {
+  //     throw error;
+  //   }
   // }
+
+  async getCountries(lang: string) {
+    return new Promise((resolve, reject) => {
+      destination
+        .then((destinationObject) => {
+          const url = destinationObject.url + lang;
+
+          this.httpService(url)
+            .then((data) => {
+              console.log('0');
+              resolve(data);
+            })
+            .catch((error) => {
+              console.log('1');
+              reject(error.response.data);
+            });
+        })
+        .catch((err) => {
+          console.log('2');
+          reject(err);
+        });
+    });
+  }
 
   async httpService(url: string): Promise<any> {
     try {
